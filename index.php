2e0147305875fda6a7f095de82aaf4d7fbddddbe
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-10-21 16:33:27 +0200

    database

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include __DIR__ . '/database/data.php';
+// var_dump($database[0]["title"])
+// echo $database[0]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-ajax-dischi</title>
+</head>
+<body>
+    
+</body>
+</html>
